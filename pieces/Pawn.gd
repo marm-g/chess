a818995168e4d49a206a1c992118b5687c576d15
Reno DuBois <me@renodubois.com>
2022-07-29 13:08:41 -0500

    start a basic click movement system (not working yet)

--- pieces/Pawn.gd
+++ pieces/Pawn.gd
@@ -1,2 +1,11 @@
+# TODO(reno): This needs to be not a rigid body.
+# They're not good for moving from point A to point B,
+# so I think I want a KinematicBody instead.
 extends RigidBody
+export var isActive = 0
+
+func _input(event):
+	if event is InputEventMouseButton:
+		print("Mouse click/unclick: ", event.position)
+
 
